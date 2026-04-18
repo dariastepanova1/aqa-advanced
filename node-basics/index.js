94cdd2735f53676120beab8f49fcdc61501814ef
Daria Stepanova <testdasha1@gmail.com>
2026-04-18 13:25:20 +0300

    add hw 3

---
+++ node-basics/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('I am blue coloured text!'));
+console.log(chalk.red('I am red coloured text!'));
+console.log(chalk.green('I am green coloured text!'));
+console.log(chalk.yellow('I am yellow coloured text!'));
