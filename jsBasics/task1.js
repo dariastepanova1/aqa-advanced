56ae482de3b570e0ade69626ef7bc37d049c5f25
dariastepanova1 <69265921+dariastepanova1@users.noreply.github.com>
2026-04-22 15:42:23 +0300

    Add hw 4 (#3)
    * add hw 4
    
    * edit task 1
    
    ---------
    
    Co-authored-by: Daria Stepanova <testdasha1@gmail.com>

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const name = "Benjamin";
+const age = 18;
+const isStudent = true;
+const height = null;
+const weight = undefined;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Height:", height);
+console.log("Weight:", weight);
---
+++ jsBasics/task4.js
@@ -0,0 +1,19 @@
+//4.1
+const circleRadius = 5;
+const circleArea = Math.PI * circleRadius ** 2;     
+
+console.log(circleArea.toFixed(2));
+
+//4.2
+const length = 10;
+const width = 5;
+const rectangleArea = length * width;
+
+console.log(rectangleArea);
+
+//4.3
+const cylinderRadius = 3;
+const cylinderHeight = 7;
+const cylinderVolume = Math.PI * cylinderRadius ** 2 * cylinderHeight;
+
+console.log(cylinderVolume.toFixed(2));
