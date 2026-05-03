18eb8d216b7555f854be078a355d9456b9adc692
Daria Stepanova <testdasha1@gmail.com>
2026-05-03 14:38:18 +0300

    add hw 7

---
+++ lesson-7/task3.js
@@ -0,0 +1,37 @@
+const divide = function (numerator, denominator) {
+    if (denominator === 0) {
+        return 'Error: Denominator cannot be zero.';
+    }
+    if (isNaN(numerator) || isNaN(denominator)) {
+        return 'Error: Both numerator and denominator must be numbers.';
+    }
+    return numerator / denominator;
+};
+
+console.log(divide(10, 2)); 
+console.log(divide(2, 0));
+console.log(divide("a",2));
+
+try {
+    divide(10, 2);
+} catch (error) {
+    console.log(error.message);
+} finally {
+    console.log('Робота завершена');
+}
+
+try {
+    divide(10, 0);
+} catch (error) {
+    console.log(error.message);
+} finally {
+    console.log('Робота завершена');
+}
+
+try {
+    divide("a", 2);
+} catch (error) {
+    console.log(error.message);
+} finally {
+    console.log('Робота завершена');
+}
